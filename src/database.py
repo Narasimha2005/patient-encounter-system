9a48396c82d9b7cf56724b0f8f47cc8090f12627
Narasimha2005 <narasimha19042005@gmail.com>
2026-02-04 16:49:04 +0530

    change mysql to sqlite

--- src/database.py
+++ src/database.py
@@ -2,32 +2,15 @@
 Database
 """
 
-import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 
-from dotenv import load_dotenv
-from sqlalchemy import (
-    create_engine,
-)
-
-# loading variables from .env file
-load_dotenv()
-
-from sqlalchemy.orm import DeclarativeBase, sessionmaker  # noqa: E402
-
-# =========================
-# DATABASE ENGINE
-# =========================
-DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(
-    DATABASE_URL,
+    "sqlite:///./mems.db",
     echo=True,  # Shows generated SQL (useful for learning)
 )
 
 SessionLocal = sessionmaker(bind=engine)
-
-# =========================
-# BASE CLASS
-# =========================
 
 
 class Base(DeclarativeBase):
